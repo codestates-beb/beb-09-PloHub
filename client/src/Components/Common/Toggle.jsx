8f28553e050094e76ab5ced34af9713434a99b4d
KimSeoYeon23 <sollucidus0221@naver.com>
2023-07-05 00:17:54 +0900

    Update Nav & Post list design

--- client/src/Components/Common/Toggle.jsx
+++ client/src/Components/Common/Toggle.jsx
@@ -1,13 +1,19 @@
 // Toggle.js
 import React from 'react';
 import { FiMenu, FiX } from 'react-icons/fi';
+import styles from '../../styles/Toggle.module.css'
 
 const Toggle = ({ isOn, handleToggle }) => {
     return (
         <div className={
-            `absolute
+            `w-12
+            h-12
+            flex 
+            justify-center 
+            items-center 
+            relative
             top-8 
-            left-8 
+            left-12 
             border
             p-2
             rounded
@@ -19,9 +25,9 @@ const Toggle = ({ isOn, handleToggle }) => {
             transform ${isOn ? 'translate-x-64' : 'translate-x-0'} ...`
         } onClick={handleToggle}>
         {isOn ? (
-            <FiX style={{ fontSize: '1.5rem', color: '#999' }} />
+            <FiX className={styles.fix}  style={{ fontSize: '1.5rem', color: '#999' }} />
         ) : (
-            <FiMenu style={{ fontSize: '1.5rem', color: '#999' }} />
+            <FiMenu className={styles.fix} style={{ fontSize: '1.5rem', color: '#999' }} />
         )}
         </div>
     );
--- client/src/Components/Layout/DefaultLayout.jsx
+++ client/src/Components/Layout/DefaultLayout.jsx
@@ -3,13 +3,11 @@ import { Header, Footer } from "../Reference/index";
 
 const DefaultLayout = ({ children }) => {
     return (
-        <>
+        <div className='max-h-screen'>
             <Header />
-                <main className="relative top-0 left-0"> {/* mt-20을 추가하여 Header와의 여백을 주고, relative를 추가하여 내부 요소의 위치를 조정할 수 있게 합니다. */}
-                    {children}
-                </main>
+                {children}
             <Footer />
-        </>
+        </div>
     )
 }
 
