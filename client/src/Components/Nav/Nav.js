01bf16d9992790017627aa67aafcfb57b5cb0122
KimSeoYeon23 <sollucidus0221@naver.com>
2023-07-04 16:27:34 +0900

    Add Nav Component

--- client/src/Components/Nav/Nav.js
+++ client/src/Components/Nav/Nav.js
@@ -1,33 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 
-const Navbar = () => {
-  return (
-    <nav className='bg-gray-500 w-40 h-screen'>
-      <ul className='flex flex-col justify-center items-center gap-5 py-8'>
-        <li>
-          <Link href="/">
-            All
-          </Link>
-        </li>
-        <li>
-          <Link href="/about">
-            행사 정보
-          </Link>
-        </li>
-        <li>
-          <Link href="/contact">
-            코스 정보
-          </Link>
-        </li>
-        <li>
-          <Link href="/contact">
-            참여 후기
-          </Link>
-        </li>
-      </ul>
-    </nav>
-  );
+const Navbar = ({ isOpen }) => {
+
+    return (
+        <nav className={
+            `absolute 
+            top-0 
+            transition-all 
+            duration-500 
+            ease-in-out 
+            bg-green-yellow 
+            w-64 h-screen 
+            transform ${isOpen ? 'translate-x-0' : '-translate-x-full'}`
+        }>
+            <ul className='flex flex-col justify-center items-center py-8'>
+            <li className='hover:bg-green-khaki hover:text-white font-bold text-lg w-full flex items-center justify-center h-16 transition-all duration-200'>
+                <Link href='/all'>All</Link>
+            </li>
+            <li className='hover:bg-green-khaki hover:text-white font-bold text-lg w-full flex items-center justify-center h-16 transition-all duration-200'>
+                <Link href='/eventinfo'>행사 정보</Link>
+            </li>
+            <li className='hover:bg-green-khaki hover:text-white font-bold text-lg w-full flex items-center justify-center h-16 transition-all duration-200'>
+                <Link href='/courseinfo'>코스 정보</Link>
+            </li>
+            <li className='hover:bg-green-khaki hover:text-white font-bold text-lg w-full flex items-center justify-center h-16 transition-all duration-200'>
+                <Link href='/review'>참여 후기</Link>
+            </li>
+            </ul>
+        </nav>
+    );
 };
 
 export default Navbar;
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,11 +1,20 @@
+import React, { useState } from 'react'
 import DefaultLayout from '../Components/Layout/DefaultLayout'
 import Nav from '../Components/Nav/Nav';
+import { Toggle } from '../Components/Reference';
 
 
 export default function Home() {
+    const [isNavOpen, setIsNavOpen] = useState(false);
+
+    const handleToggle = () => {
+        setIsNavOpen(!isNavOpen);
+    };
+
     return (
         <DefaultLayout>
-            <Nav />
+            <Toggle isOn={isNavOpen} handleToggle={handleToggle} />
+            <Nav isOpen={isNavOpen} />
         </DefaultLayout>
     )
 }
