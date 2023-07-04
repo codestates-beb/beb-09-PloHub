a1202704f3112d3720c7114d115ec677a9d3af2a
KimSeoYeon23 <sollucidus0221@naver.com>
2023-07-05 00:55:08 +0900

    Add signup.js

---
+++ client/src/pages/signup.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Header, Footer } from '../Components/Reference'
+
+const signup = () => {
+    return (
+        <>
+            <Header />
+                회원가입
+            <Footer />
+        </>
+    )
+}
+
+export default signup;
