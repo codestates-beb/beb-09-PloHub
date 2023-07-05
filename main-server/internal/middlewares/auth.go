da792d23c938251b6aca8abf7719d4e26ac5d742
Lee Hyohwak <crewe1746@naver.com>
2023-07-05 14:43:37 +0900

    Add middleware to verify access token

---
+++ main-server/internal/middlewares/auth.go
@@ -0,0 +1,48 @@
+package middlewares
+
+import (
+	"context"
+	"main-server/internal/models"
+	"main-server/internal/services/auth"
+	"net/http"
+	"strings"
+)
+
+func AccessTokenRequired(authSrv auth.Service) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Add("Vary", "Authorization")
+
+			authorization := r.Header.Get("Authorization")
+
+			if authorization == "" {
+				http.Error(w, "Unauthorized", http.StatusUnauthorized)
+				return
+			}
+
+			authParts := strings.Split(authorization, " ")
+			if len(authParts) != 2 {
+				http.Error(w, "Unauthorized", http.StatusUnauthorized)
+				return
+			}
+
+			if authParts[0] != "Bearer" {
+				http.Error(w, "Unauthorized", http.StatusUnauthorized)
+				return
+			}
+
+			token := authParts[1]
+
+			user, err := authSrv.VerifyToken(token, models.TokenRoleAccess)
+			if err != nil {
+				http.Error(w, "Unauthorized", http.StatusUnauthorized)
+				return
+			}
+
+			ctx := r.Context()
+			ctx = context.WithValue(ctx, "UserID", user.ID)
+
+			next.ServeHTTP(w, r.WithContext(ctx))
+		})
+	}
+}
