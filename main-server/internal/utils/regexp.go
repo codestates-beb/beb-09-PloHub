bb4f99e4b15994766ca1df4aa5ae06de2ebdbd89
Lee Hyohwak <crewe1746@naver.com>
2023-07-06 11:01:54 +0900

    Update email and password validation function

--- main-server/internal/utils/regexp.go
+++ main-server/internal/utils/regexp.go
@@ -1,16 +1,58 @@
 package utils
 
-import "regexp"
-
-const (
-	EmailRegex    = `^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$`
-	PasswordRegex = `/^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$/`
+import (
+	"errors"
+	"regexp"
 )
 
-func ValidateEmail(email string) (bool, error) {
-	return regexp.MatchString(EmailRegex, email)
+const (
+	EmailRegex = `^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$`
+)
+
+func ValidateEmail(email string) error {
+	valid, err := regexp.MatchString(EmailRegex, email)
+	if err != nil {
+		return err
+	}
+
+	if !valid {
+		return errors.New("invalid email")
+	}
+
+	return nil
 }
 
-func ValidatePassword(password string) (bool, error) {
-	return regexp.MatchString(PasswordRegex, password)
+func ValidatePassword(password string) error {
+	if len(password) < 8 {
+		return errors.New("password must be at least 8 characters long")
+	}
+
+	valid, err := regexp.MatchString(`([a-z])+`, password)
+	if err != nil {
+		return err
+	}
+
+	if !valid {
+		return errors.New("password must contain at least one lowercase letter")
+	}
+
+	valid, err = regexp.MatchString(`([A-Z])+`, password)
+	if err != nil {
+		return err
+	}
+
+	if !valid {
+		return errors.New("password must contain at least one uppercase letter")
+	}
+
+	valid, err = regexp.MatchString("([!@#$%^&*.?-])+", password)
+	if err != nil {
+		return err
+	}
+
+	if !valid {
+		return errors.New("password must contain at least one special character")
+	}
+
+	return nil
 }
