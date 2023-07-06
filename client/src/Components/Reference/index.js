8827708c9c9b3b8ed5ed4bbba5ebba7fef7adef0
KimSeoYeon23 <sollucidus0221@naver.com>
2023-07-07 00:59:07 +0900

    Add NFT Detail Page

--- client/src/Components/Reference/index.js
+++ client/src/Components/Reference/index.js
@@ -6,7 +6,12 @@ import ModalLayout from "../Common/Modal";
 import logoBlack from '../../assets/image/logo_black.png'
 import logoWhite from '../../assets/image/logo_white.png'
 import ploHub from '../../assets/image/PloHub.png';
+import dummy1 from '../../assets/image/dummy_1.jpg';
+import dummy2 from '../../assets/image/dummy_2.jpg';
+import dummy3 from '../../assets/image/dummy_3.jpg';
+
 
 export {
-    Header, Footer, Toggle, ModalLayout, logoBlack, logoWhite, ploHub
+    Header, Footer, Toggle, ModalLayout, 
+    logoBlack, logoWhite, ploHub, dummy1, dummy2, dummy3
 }
