2bccbb68bd2dafff058186c5344a3fd367b7fd0f
Lee Hyohwak <crewe1746@naver.com>
2023-07-09 17:47:27 +0900

    Add function to initialize aws s3 client

---
+++ main-server/internal/loaders/s3.go
@@ -0,0 +1,30 @@
+package loaders
+
+import (
+	"context"
+	"main-server/internal/configs"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"go.uber.org/zap"
+)
+
+func MustInitS3Client(ctx context.Context, cfg *configs.Config) *s3.Client {
+	s3Client, err := NewS3Client(ctx, cfg)
+	if err != nil {
+		zap.L().Panic("Failed to init s3 client", zap.Error(err))
+	}
+
+	return s3Client
+}
+
+func NewS3Client(ctx context.Context, cfg *configs.Config) (*s3.Client, error) {
+	creds := credentials.NewStaticCredentialsProvider(cfg.S3.AccessKey, cfg.S3.SecretKey, "")
+	s3cfg, err := config.LoadDefaultConfig(ctx, config.WithCredentialsProvider(creds), config.WithRegion(cfg.S3.Region))
+	if err != nil {
+		return nil, err
+	}
+
+	return s3.NewFromConfig(s3cfg), nil
+}
