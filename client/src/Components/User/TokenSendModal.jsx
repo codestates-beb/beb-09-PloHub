f69a21bd1be87ba5c6b36001a458ff833a45524f
KimSeoYeon23 <sollucidus0221@naver.com>
2023-07-13 08:37:04 +0900

    Update Token Send

--- client/src/Components/User/TokenSendModal.jsx
+++ client/src/Components/User/TokenSendModal.jsx
@@ -1,12 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
+import axios from 'axios';
+import cookie from 'cookie';
 import { FaAddressCard } from 'react-icons/fa'
-import { ploHub } from '../Reference'
+import { ploHub, ModalLayout } from '../Reference'
 
 const TokenSendModal = ({ setIsModalOpen }) => {
+    const [toAddr, setToAddr] = useState('');
+    const [tokenAmount, setTokenAmount] = useState(0);
+    const [modalOpen, setModalOpen] = useState('');
+    const [modalTitle, setModalTitle] = useState('');
+    const [modalBody, setModalBody] = useState('');
+
+
+    const tokenSend = async () => {
+        const token = cookie.parse(document.cookie || '');
+        const formData = new FormData();
+
+        formData.append('to_address', toAddr);
+        formData.append('token_amount', tokenAmount);
+
+        try {
+            const response = await axios.post(`${process.env.NEXT_PUBLIC_BACKEND_URL}/token/transfer`, formData, {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                },
+                withCredentials: true
+            });
+
+            console.log('response', response);
+            if (response.status === 200) {
+                setModalOpen(true);
+                setModalTitle('Success');
+                setModalBody('토큰 전송이 완료되었습니다.');
+
+                setTimeout(() => {
+                    setModalOpen(false);
+                    router.reload();
+                }, 3000);
+            }
+            console.log(response);
+        } catch (error) {
+            console.log(error);
+            setModalOpen(true);
+            setModalTitle('Error');
+            setModalBody(error.message);
+
+            setTimeout(() => {
+                setModalOpen(false);
+            }, 3000);
+        }
+
+    }
     return (
         <>
-            <form className='
+            <div className='
                 flex 
                 flex-col 
                 justify-center 
@@ -23,7 +71,8 @@ const TokenSendModal = ({ setIsModalOpen }) => {
                         rounded-lg 
                         w-full
                         p-1'
-                        type="text" />
+                        type="text"
+                        onChange={(e) => setToAddr(e.target.value)} />
                 </div>
                 <div className='w-full flex flex-col justify-center gap-3'>
                     <div className='flex items-center gap-3'>
@@ -35,7 +84,8 @@ const TokenSendModal = ({ setIsModalOpen }) => {
                         rounded-lg 
                         w-full
                         p-1'
-                        type="number" />
+                        type="number"
+                        onChange={(e) => setTokenAmount(e.target.value)}/>
                 </div>
                 <div className='w-[60%] flex justify-center items-center gap-3'>
                     <button className='
@@ -62,11 +112,12 @@ const TokenSendModal = ({ setIsModalOpen }) => {
                         hover:bg-blue-dark 
                         transition 
                         duration-300' 
-                        type='submit'>
+                        onClick={tokenSend}>
                         전송
                     </button>
                 </div>
-            </form>
+            </div>
+            <ModalLayout isOpen={modalOpen} modalTitle={modalTitle} modalBody={modalBody} />
         </>
     )
 }
